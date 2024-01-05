37efc5dd720b9bbf9637ec282d745aab61a787bc
Ryder <ryder@Ryders-MBP.attlocal.net>
2024-01-05 14:45:19 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+mod commands;
+mod model;
+
+use poise::serenity_prelude as serenity;
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
+    let intents = serenity::GatewayIntents::non_privileged();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions{
+                commands: vec![],
+                ..Default::default()
+            })
+        .setup(|ctx, _ready, framework|{
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+    client.unwrap().start().await.unwrap();
+}
